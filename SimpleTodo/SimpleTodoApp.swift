ec0edb730492e47387305cea65002d57b801f13e
yongjun18 <yongjun18976@gmail.com>
2023-06-07 15:38:25 +0900

    git 초기 세팅

---
+++ SimpleTodo/SimpleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoApp.swift
+//  SimpleTodo
+//
+//  Created by yongjun18 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
