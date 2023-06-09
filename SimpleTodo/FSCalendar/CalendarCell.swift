f5569b9abe142c08f89ca3170bcc176dde0a201e
yongjun18 <yongjun18976@gmail.com>
2023-06-09 10:03:38 +0900

    정해진 비율 유지하면서 그림자 생성되도록 변경

--- SimpleTodo/FSCalendar/CalendarCell.swift
+++ SimpleTodo/FSCalendar/CalendarCell.swift
@@ -10,6 +10,10 @@ import FSCalendar
 
 class CalendarCell: FSCalendarCell {
     weak var selectIndicatorView: UIView!
+    
+    var sizeRatio: Double = 0.85
+    var cornerRatio: Double = 0.1
+    var shadowRatio: Double = 0.05
  
     required init!(coder aDecoder: NSCoder!) {
         fatalError("init(coder:) has not been implemented")
@@ -18,16 +22,19 @@ class CalendarCell: FSCalendarCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        let selectIndicatorView = UIView(frame: CGRect(x: 5, y: 5, width: frame.width-10, height: frame.height-10))
+        let selectIndicatorView = UIView(frame: CGRect(x: frame.width * (1.0-sizeRatio) * 0.5,
+                                                       y: frame.height * (1.0-sizeRatio) * 0.5,
+                                                       width: frame.width * sizeRatio,
+                                                       height: frame.height * sizeRatio))
         selectIndicatorView.backgroundColor = UIColor(.white)
-        selectIndicatorView.layer.cornerRadius = 4
+        selectIndicatorView.layer.cornerRadius = frame.height * cornerRatio
         
         // 그림자 설정
         selectIndicatorView.layer.shadowOffset = CGSize(width: 0, height: 0)
         selectIndicatorView.layer.shadowOpacity = 0.16
-        selectIndicatorView.layer.shadowRadius = 3
+        selectIndicatorView.layer.shadowRadius = frame.height * shadowRatio
         
-        self.contentView.insertSubview(selectIndicatorView, at: 0)
+        self.contentView.insertSubview(selectIndicatorView, at: 1)
         self.selectIndicatorView = selectIndicatorView
         
         // 날짜 선택 시 나타나는 원형 효과 제거
@@ -37,8 +44,11 @@ class CalendarCell: FSCalendarCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.selectIndicatorView.bounds = CGRect(x: 5, y: 5,
-                                                 width: self.contentView.bounds.width-10,
-                                                 height: contentView.bounds.height-10)
+        self.selectIndicatorView.bounds = CGRect(x: frame.width * (1.0-sizeRatio) * 0.5,
+                                                 y: frame.height * (1.0-sizeRatio) * 0.5,
+                                                 width: frame.width * sizeRatio,
+                                                 height: frame.height * sizeRatio)
+        self.selectIndicatorView.layer.cornerRadius = frame.height * cornerRatio
+        selectIndicatorView.layer.shadowRadius = frame.height * shadowRatio
     }
 }
