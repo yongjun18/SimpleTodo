05066b1f53354e116ee5eeb3986dd0a187e92591
yongjun18 <yongjun18976@gmail.com>
2023-06-09 12:25:10 +0900

    불필요하게 중복된 코드 제거

--- SimpleTodo/FSCalendar/CalendarCell.swift
+++ SimpleTodo/FSCalendar/CalendarCell.swift
@@ -27,21 +27,13 @@ class CalendarCell: FSCalendarCell {
                                                        width: frame.width * sizeRatio,
                                                        height: frame.height * sizeRatio))
         selectIndicatorView.backgroundColor = UIColor(.white)
-        selectIndicatorView.layer.cornerRadius = frame.height * cornerRatio
         
         // 그림자 설정
         selectIndicatorView.layer.shadowOffset = CGSize(width: 0, height: 0)
         selectIndicatorView.layer.shadowOpacity = 0.25
-        selectIndicatorView.layer.shadowRadius = frame.height * shadowRatio
         
         self.contentView.insertSubview(selectIndicatorView, at: 1)
         self.selectIndicatorView = selectIndicatorView
-        
-        self.titleLabel.frame = CGRect(x: selectIndicatorView.frame.origin.x,
-                                       y: selectIndicatorView.frame.origin.y,
-                                       width: selectIndicatorView.frame.width,
-                                       height: selectIndicatorView.frame.height * (CalendarCustom.designedTitleLabelHeight / CalendarCustom.designedCellHeight))
-        
         
         // 날짜 선택 시 나타나는 원형 효과 제거
         self.shapeLayer.isHidden = true
