8c853e65f43c8fe84c6115e339801a574248a29f
yongjun18 <yongjun18976@gmail.com>
2023-12-30 00:34:03 +0900

    툴팁 배경 색상 변경

--- SimpleTodo/View/CatContainerView.swift
+++ SimpleTodo/View/CatContainerView.swift
@@ -98,7 +98,7 @@ struct CatScreenView: View {
 }
 
 struct CatToolTipView: View {
-    let toolTipColor = Color(uiColor: UIColor(red: 242.0/255.0, green: 120.0/255.0, blue: 182.0/255.0, alpha: 1.0))
+    let toolTipColor = Color(uiColor: UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 0.8))
     
     var body: some View {
         VStack(alignment: .trailing, spacing: 0) {
